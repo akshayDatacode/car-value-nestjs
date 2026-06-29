c444d807c62536a0b175405771158a0a44d219a0
akshayDatacode <akshay.webllisto@gmail.com>
2026-06-30 00:31:19 +0530

    Handle Auth Gaurd setup and authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,55 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 export class UsersController {
-
+  
   constructor(
     private userService: UsersService,
     private authService: AuthService
   ) { }
 
+  //  Session Cookie Learning
+
+  @Get('/color/:color')
+  setColor(@Param('color') color: string, @Session() session: any) {
+    session.color = color
+  }
+  
+  @Get('/color')
+  getColor(@Session() session: any) {
+    return session
+  }
+  
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password)
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password)
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password)
+    session.userId = user.id
+    return user
+  }
+
+  @Post('/signout')
+  async signout(@Session() session: any) {
+    session.userId = null
   }
 
   @Get('/:id')
